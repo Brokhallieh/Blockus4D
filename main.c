746a1c6604e06eb894bf5cd09f5321706f2575e7
TOUSSAINT YANN <yann.toussaint2@etu.unistra.fr>
2023-02-09 00:27:54 +0100

    Retenue des pièces utilisées

--- main.c
+++ main.c
@@ -2,15 +2,28 @@
 #include <stdbool.h>
 
 
-#define TAILLE_CELL 5	// doit faire au moins 5
-#define NB_PIECES 37    // à titre indicatif (ne pas modifier)
-#define NB_PLAYERS 2    // à titre indicatif
+#define TAILLE_CELL 5	// Taille de chaque dimension de la matrice de matrice, doit faire au moins 5
+#define NB_PIECES 36    // nombre de pièces dans le jeu, donné à titre indicatif (ne pas modifier)
+#define NB_PLAYERS 2    // Nombre de joueurs, donné à titre indicatif
+#define END_CHAR 'n'	// indique le caractère que l'on doit entrer pour arreter de jouer quand on nous pose la question
 
 
-void _2d_fill_with_b(int dim1_taille, int dim2_taille, int tab[dim1_taille][dim2_taille], bool b) {
+void _2d_fill_with_b(int dim1_taille, int dim2_taille, bool tab[dim1_taille][dim2_taille], bool b) {
 	for (int i=0; i<dim1_taille; i++) {
 		for (int j=0; j<dim2_taille; j++) {
 			tab[i][j] = b;
+		}
+	}
+}
+
+void _4d_fill_with_n(int taille, int tab[taille][taille][taille][taille], int n) {
+	for (int i=0; i<taille; i++) {
+		for (int j=0; j<taille; j++) {
+			for (int k=0; k<taille; k++) {
+				for (int l=0; l<taille; l++) {
+					tab[i][j][k][l] = n;
+				}
+			}
 		}
 	}
 }
@@ -27,13 +40,13 @@ void _4d_fill_with_b(int taille, int tab[taille][taille][taille][taille], bool b
 	}
 }
 
-void _5d_fill_with_n(int taille_4_prem_dim, int taille_5eme_dim, int tab[taille_4_prem_dim][taille_4_prem_dim][taille_4_prem_dim][taille_4_prem_dim][taille_5eme_dim], int n) {
+void _5d_fill_with_b(int taille_4_prem_dim, int taille_5eme_dim, int tab[taille_4_prem_dim][taille_4_prem_dim][taille_4_prem_dim][taille_4_prem_dim][taille_5eme_dim], bool b) {
 	for (int i=0; i<taille_4_prem_dim; i++) {
 		for (int j=0; j<taille_4_prem_dim; j++) {
 			for (int k=0; k<taille_4_prem_dim; k++) {
 				for (int l=0; l<taille_4_prem_dim; l++) {
 					for (int m=0; m<taille_5eme_dim; m++) {
-						tab[i][j][k][l][m] = n;
+						tab[i][j][k][l][m] = b;
 					}
 				}
 			}
@@ -63,6 +76,7 @@ void affiche_sur_terminal_4d(int taille, int tab[taille][taille][taille][taille]
 			printf("\n\n");
 		}
 	}
+	printf("\n");
 }
 
 void affiche_sur_terminal_2d(int dim1_taille, int dim2_taille, int tab[dim1_taille][dim2_taille]) {
@@ -80,7 +94,7 @@ void affiche_sur_terminal_2d(int dim1_taille, int dim2_taille, int tab[dim1_tail
 }
 
 void init_pieces(int taille, int nb_pieces, int pieces[taille][taille][taille][taille][nb_pieces]) {
-	_5d_fill_with_n(TAILLE_CELL, NB_PIECES-1, pieces, 0);
+	_5d_fill_with_b(TAILLE_CELL, NB_PIECES-1, pieces, false);
 
 	//pièce de longueur 1:
 	pieces[0][0][0][0][0] = true;
@@ -287,10 +301,38 @@ void init_pieces(int taille, int nb_pieces, int pieces[taille][taille][taille][t
 	
 }
 
+bool between(int bottom, int n, int top) {
+	return (bottom <= n) && (n <= top);
+}
+
+void affiche_pieces_non_utilisees(int dim1_taille, int dim2_taille, bool tab[dim1_taille][dim2_taille], int joueur) {
+	printf("Piece(s) non utilisee(s): ");
+	int premiere_piece_non_utilisee = -1;
+	int i=0;
+	while (premiere_piece_non_utilisee == -1) {
+		if (i > dim1_taille) {
+			break;
+		}
+		else if (!tab[i][joueur-1]) {
+			premiere_piece_non_utilisee = i;
+		}
+		i++;
+	}
+	if (premiere_piece_non_utilisee != -1) {
+		printf("%d", premiere_piece_non_utilisee+1);
+	}
+	for (i=premiere_piece_non_utilisee+1; i< dim1_taille; i++) {
+		if (!tab[i][joueur-1]) {
+			printf(", %d", i+1);
+		}
+	}
+	printf("\n");
+}
+
 
 int main() {
 	int board[TAILLE_CELL][TAILLE_CELL][TAILLE_CELL][TAILLE_CELL];
-	_4d_fill_with_b(TAILLE_CELL, board, 0);	/*board contient l'état actuel du plateau (1 si pièce du J1
+	_4d_fill_with_n(TAILLE_CELL, board, 1094257386);	/*board contient l'état actuel du plateau (1 si pièce du J1
 		présente, 2 si pièce du J2 présente, 3 si tentative de pose et que pièce du J1 présente, et 4 si
 		tentative de pose et que pièce du J2 présente, et 0 sinon)*/
 	int player1[TAILLE_CELL][TAILLE_CELL][TAILLE_CELL][TAILLE_CELL];
@@ -305,13 +347,11 @@ int main() {
 	board[1][1][1][1] = 1;
 	player2[TAILLE_CELL-2][TAILLE_CELL-2][TAILLE_CELL-2][TAILLE_CELL-2] = 1;
 	board[TAILLE_CELL-2][TAILLE_CELL-2][TAILLE_CELL-2][TAILLE_CELL-2] = 2;
-
-	// dit si chaque pièce a déja étée utilisée par le J1 et le J2 (la pièce d'indice (NB_PIECES-1(36)) est juste un "flag")
-	int listepieces[NB_PIECES][NB_PLAYERS];
+	
+	bool listepieces[NB_PIECES][NB_PLAYERS];	// listepieces dit si chaque pièce a déja étée utilisée par le J1 et le J2
 	_2d_fill_with_b(NB_PIECES, NB_PLAYERS, listepieces, false);
-	listepieces[NB_PIECES-1][0/*P1*/] = true; listepieces[NB_PIECES-1][1/*P2*/] = true;
 
-	// affiche_sur_terminal_2d(NB_PIECES, 2, listepieces);	//affichage de débugage
+	//affiche_sur_terminal_2d(NB_PIECES, 2, listepieces);	//affichage de débugage
 
 	int score[NB_PLAYERS] = {0, 0};	//indique le score du J1, puis du J2
 	bool inGame[NB_PLAYERS] = {true, true};	//indique si le J1, puis le J2, joue toujours
@@ -319,6 +359,34 @@ int main() {
 	int pieces[TAILLE_CELL][TAILLE_CELL][TAILLE_CELL][TAILLE_CELL][NB_PIECES];
 	init_pieces(TAILLE_CELL, NB_PIECES, pieces);	//contient les emlacements de base de chaque pièce
 
-	
+	int playernumber = 1;	//initialise le tour au J1
+
+	while (inGame[0] || inGame[1]) {	//tant qu'1 des 2 J joue toujours
+		if (inGame[playernumber-1]) {	//vérifie si le joueur dont c'est le tour joue toujours, passe son tour sinon
+			printf("ready, player %d\n", playernumber);	//affichage de à qui c'est le tour
+			affiche_pieces_non_utilisees(NB_PIECES, NB_PLAYERS, listepieces, playernumber);
+			int piecenumber;	//contient la pièce que le numéro de la pièce que le joueur veut jouer
+			do {
+				printf("Veuillez donnez le numero de la piece que vous voulez mettre:");
+				scanf("%d", &piecenumber);
+				printf("\n");
+				if (listepieces[piecenumber-1][playernumber-1]) {	//si la pièce a déja étée utilisée
+					printf("Piece %d deja utilisee.\n", piecenumber);
+					affiche_pieces_non_utilisees(NB_PIECES, NB_PLAYERS, listepieces, playernumber);
+				}
+				else  if (between(1, piecenumber, NB_PIECES)) {	//marque la pièce comme utilisée si elle est choisie par le joueur et est valide
+					listepieces[piecenumber-1][playernumber-1] = true;
+					break;	//sors de la boucle while car on a le numéro de pièce recherché
+				}
+			} while (!between(1, piecenumber, NB_PIECES) || (listepieces[piecenumber-1][playernumber-1]));	//recommence si le numéro donné n'est pas valide
+		
+			printf("état actuel du plateau\n");
+			affiche_sur_terminal_4d(TAILLE_CELL, board); printf("\n");
+		}
+
+
+
+		if (playernumber == 1) {playernumber=2;} else {playernumber=1;}	//switch de joueur
+	}
 	return 0;
 }
